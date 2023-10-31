77d7d0a92c515b5cd2bca72bd7ea0f59a7c8eb76
YCHuang2112 <96855801+YCHuang2112@users.noreply.github.com>
2023-11-01 06:29:18 +0800

    second

---
+++ test.py
@@ -0,0 +1,21 @@
+import python 
+
+
+def test():
+    # Create a new instance of the Python class
+    p = python.Python()
+
+    # Call the method
+    p.method()
+
+    # Get the attribute
+    print(p.attribute)
+
+    # Set the attribute
+    p.attribute = "new value"
+
+    # Get the attribute
+    print(p.attribute)
+
+# Call the method
+p.method()
